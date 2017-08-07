053cf0df8f8de4c89a1f0d6da6e8431e77997201
Sanek-OS9 <Sanek_OS9@mail.ru>
2017-08-08 02:25:25 +0300

    #update так будет правильней

--- Core/Authorize.php
+++ Core/Authorize.php
@@ -2,14 +2,19 @@
 namespace Core;
 
 abstract class Authorize{
+    // ключ, с которым будет зранится пользовательский ID
+    const KEY_USER_ID = 'id';
+    // ключ, с которым будет зранится пользовательский хэш пароля 
+    const KEY_USER_PASSWORD = 'what_is_it';
+
     # авторизируемся
     public static function authorized(int $id_user, string $hash_pass)
     {
         if (self::isAuthorize()) {
             return;
         }
-        $_SESSION['id'] = $id_user;
-        $_SESSION['what_is_it'] = $hash_pass;
+        $_SESSION[self::KEY_USER_ID] = $id_user;
+        $_SESSION[self::KEY_USER_PASSWORD] = $hash_pass;
     }
     # проверям авторизованы ли
     public static function isAuthorize(): bool
@@ -19,17 +24,17 @@ abstract class Authorize{
     # получаем ID пользователя
     public static function getId(): int
     {
-        return $_SESSION['id'] ?? 0;
+        return $_SESSION[self::KEY_USER_ID] ?? 0;
     }
     # получаем хэш его пароля
     public static function getHash(): string
     {
-        return $_SESSION['what_is_it'] ?? 0;
+        return $_SESSION[self::KEY_USER_PASSWORD] ?? 0;
     }
     # выходим с авторизации
     public static function exit()
     {
-        unset($_SESSION['id']);
-        unset($_SESSION['what_is_it']);
+        unset($_SESSION[self::KEY_USER_ID]);
+        unset($_SESSION[self::KEY_USER_PASSWORD]);
     }
 }
